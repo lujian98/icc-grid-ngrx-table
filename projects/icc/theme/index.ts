357b73fb5f8371f3314944b61c06ceba0b837c56
Jimmy Lu <jimmy.lu@sunbirddcim.com>
2024-08-09 08:10:45 -0500

    add theme

--- projects/icc/theme/index.ts
+++ projects/icc/theme/index.ts
@@ -1,4 +1,4 @@
-export * from './public-api';
+//export * from './public-api';
 //export * from './src/about.module';
 //export * from './src/about.component';
 //export * from './src/components/button/button.component';
--- projects/icc/theme/public-api.ts
+++ projects/icc/theme/public-api.ts
@@ -1,5 +1,4 @@
-export * from './public-api';
 //export * from './src/about.module';
 //export * from './src/about.component';
 //export * from './src/components/button/button.component';
-
+export {};
