c3e80fffb92c627c82831f18ee60a4c7d9005a1b
Jimmy Lu <jimmy.lu@sunbirddcim.com>
2024-08-18 10:17:47 -0500

    popover menu

--- projects/icc/grid/src/components/grid-header/grid-column-menu/grid-column-menu.component.ts
+++ projects/icc/grid/src/components/grid-header/grid-column-menu/grid-column-menu.component.ts
@@ -1,55 +1,35 @@
-import { ChangeDetectionStrategy, Component, ElementRef, OnInit, Input, inject } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { ChangeDetectionStrategy, Component, Input, inject } from '@angular/core';
+import { Observable } from 'rxjs';
 import { IccGridFacade } from '../../../+state/grid.facade';
-import { IccColumnConfig, IccGridConfig, IccSortField } from '../../../models/grid-column.model';
+import { IccColumnConfig, IccGridConfig } from '../../../models/grid-column.model';
 
 @Component({
   selector: 'sun-grid-column-menu',
   templateUrl: './grid-column-menu.component.html',
   styleUrls: ['./grid-column-menu.component.scss'],
   changeDetection: ChangeDetectionStrategy.OnPush,
-  host: {
-    '(document:click)': 'onClick($event)',
-  },
   standalone: true,
   imports: [
     CommonModule,
   ],
 })
-export class IccGridColumnMenuComponent implements OnInit {
+export class IccGridColumnMenuComponent {
   private gridFacade = inject(IccGridFacade);
-  private elementRef = inject(ElementRef);
-  @Input() gridConfig!: IccGridConfig;
+  columnConfig$!: Observable<IccColumnConfig[]>;
+  private _gridConfig!: IccGridConfig;
+
+  @Input()
+  set gridConfig(val: IccGridConfig) {
+    this._gridConfig = val;
+    this.columnConfig$ = this.gridFacade.selectColumnConfig(val.gridName);
+  }
+  get gridConfig(): IccGridConfig {
+    return this._gridConfig;
+  }
   @Input() column!: IccColumnConfig;
-  constructor() { }
 
-  ngOnInit() {
-    console.log(' gridConfig=', this.gridConfig)
-    console.log(' column=', this.column)
-    this.gridFacade.selectColumnConfig(this.gridConfig.gridName).subscribe((column) => {
-      console.log(' all column=', column)
-    })
-  }
-
-  onClick(event: any) {
-    if (!this.elementRef.nativeElement.contains(event.target)) { // or some similar check
-      // this.close();
-    }
+  getTitle(column: IccColumnConfig): string {
+    return column.title === undefined ? column.name : column.title;
   }
 }
-
-/*
-@Component({
-  host: {
-    '(document:click)': 'onClick($event)',
-  },
-})
-class SomeComponent() {
-  constructor(private _eref: ElementRef) { }
-
-  onClick(event) {
-   if (!this._eref.nativeElement.contains(event.target)) // or some similar check
-     doSomething();
-  }
-}
-*/
