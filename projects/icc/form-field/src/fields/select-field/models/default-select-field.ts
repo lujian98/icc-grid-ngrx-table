4df0e497756c1d6fd1224ff1a5cd94614092e8b0
Jimmy Lu <jimmy.lu@sunbirddcim.com>
2024-09-07 08:04:29 -0500

    select state

--- projects/icc/form-field/src/fields/select-field/models/default-select-field.ts
+++ projects/icc/form-field/src/fields/select-field/models/default-select-field.ts
@@ -2,6 +2,13 @@ import { IccSelectFieldConfig, IccSelectFieldState } from './select-field.model'
 
 export const defaultSelectFieldConfig: IccSelectFieldConfig = {
   fieldName: 'icc-select',
+  selectOnly: false, // false select, true autocomplete
+  multiSelection: false,
+  value: undefined,
+  options: [],
+  optionLabel: 'name',
+  optionKey: 'title',
+  placeholder: '',
 };
 
 export const defaultSelectFieldState: IccSelectFieldState = {
--- projects/icc/form-field/src/fields/select-field/models/select-field.model.ts
+++ projects/icc/form-field/src/fields/select-field/models/select-field.model.ts
@@ -1,6 +1,13 @@
 export interface IccSelectFieldConfig {
   fieldName: string;
   urlKey?: string; // Only for remote. if not defined, use gridName
+  selectOnly: boolean; // false select, true autocomplete
+  multiSelection: boolean;
+  value: any; //array???
+  options: any[];
+  optionLabel: string;
+  optionKey: string;
+  placeholder: string;
 }
 
 export interface IccSelectFieldState<T extends object = object> {
