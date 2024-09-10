a3384381b91c9d0e6089a08cdd66be078b58f7d3
Jimmy Lu <jimmy.lu@sunbirddcim.com>
2024-09-10 11:21:20 -0500

    grid config clear filter

---
+++ projects/icc-demo/src/app/examples/grid/remote-data/grid-remote-config.component.ts
@@ -0,0 +1,19 @@
+import { CommonModule } from '@angular/common';
+import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { IccGridConfig, IccGridComponent, defaultGridConfig } from '@icc/ui/grid';
+
+@Component({
+  selector: 'app-grid-remote-config',
+  template: `<icc-grid [gridConfig]="gridConfig"></icc-grid>`,
+  styles: [':host { width: 100%; }'],
+  changeDetection: ChangeDetectionStrategy.OnPush,
+  standalone: true,
+  imports: [CommonModule, IccGridComponent],
+})
+export class AppGridRemoteConfigComponent {
+  gridConfig: IccGridConfig = {
+    ...defaultGridConfig,
+    urlKey: 'DCR',
+    remoteGridConfig: true,
+  };
+}
