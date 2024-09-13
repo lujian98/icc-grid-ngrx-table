ffd029650e8d19ec589a30879f6df1fdf46fcfc7
Jimmy Lu <lujian98@yahoo.com>
2024-09-12 22:28:06 -0500

    clean

--- projects/icc-demo/src/app/app.routes.ts
+++ projects/icc-demo/src/app/app.routes.ts
@@ -1,5 +1,4 @@
-import { NgModule } from '@angular/core';
-import { RouterModule, Routes } from '@angular/router';
+import { Routes } from '@angular/router';
 
 export const routes: Routes = [
   {
@@ -20,11 +19,3 @@ export const routes: Routes = [
     ],
   },
 ];
-
-/*
-@NgModule({
-  imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule],
-})
-export class AppRoutingModule {}
-*/
