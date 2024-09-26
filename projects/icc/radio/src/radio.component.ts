81a76ac06177d6306604a310f47a4aa202b824c3
Jimmy Lu <lujian98@yahoo.com>
2024-09-26 18:35:04 -0500

    radio

--- projects/icc/radio/src/radio.component.ts
+++ projects/icc/radio/src/radio.component.ts
@@ -1,7 +1,8 @@
-import { Component, Input, OnInit } from '@angular/core';
+import { Component, Input, OnInit, forwardRef } from '@angular/core';
 import { FormControl } from '@angular/forms';
 import { ReactiveFormsModule } from '@angular/forms';
 import { CommonModule } from '@angular/common';
+import { NG_VALUE_ACCESSOR, ControlValueAccessor } from '@angular/forms';
 
 @Component({
   selector: 'icc-radio',
@@ -9,18 +10,21 @@ import { CommonModule } from '@angular/common';
   styleUrls: ['./radio.component.scss'],
   standalone: true,
   imports: [CommonModule, ReactiveFormsModule],
+  providers: [
+    {
+      provide: NG_VALUE_ACCESSOR,
+      useExisting: forwardRef(() => IccRadioComponent),
+      multi: true,
+    },
+  ],
 })
-export class IccRadioComponent implements OnInit {
+export class IccRadioComponent implements ControlValueAccessor {
   @Input() field!: FormControl;
   @Input() fieldName!: string;
   @Input() value!: string;
   @Input() checked!: boolean;
   @Input() name!: string;
   private _disabled = false;
-
-  ngOnInit() {
-    // console.log('ctrl', this.ctrl);
-  }
 
   @Input()
   get disabled(): boolean {
@@ -29,5 +33,30 @@ export class IccRadioComponent implements OnInit {
   set disabled(disabled: boolean) {
     this._disabled = disabled;
     // this.cd.markForCheck();
+  }
+
+  registerOnChange(fn: any): void {
+    //this.onChange = fn;
+  }
+
+  registerOnTouched(fn: any): void {
+    //this.onTouched = fn;
+  }
+
+  setDisabledState?(isDisabled: boolean): void {
+    this.disabled = isDisabled;
+  }
+
+  writeValue(value: boolean): void {
+    this.checked = value;
+
+    //Fix for issue where we reference .detectChanges
+    //on a destroyed view (such as when toggling control
+    //enabled/disabled from a parent form group)
+    //https://github.com/SAP/fundamental-ngx/issues/2364
+    /*
+    if (!(this.changeDetector as any).destroyed) {
+      this.changeDetector.detectChanges();
+    }*/
   }
 }
