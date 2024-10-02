aed7cbf5b52514f99a94179632bc740d90cdb77f
Jimmy Lu <lujian98@yahoo.com>
2024-10-01 21:08:48 -0500

    form field control

--- projects/icc/form-field/src/directive/field-control.directive.ts
+++ projects/icc/form-field/src/directive/field-control.directive.ts
@@ -2,9 +2,9 @@ import { Directive, Input } from '@angular/core';
 import { FormControl } from '@angular/forms';
 
 @Directive({
-  selector: 'icc-form-field[iccFieldControl] ', // tslint:disable-line: directive-selector
+  selector: 'icc-form-field[iccFormFieldControl] ', // tslint:disable-line: directive-selector
   standalone: true,
 })
 export class IccFormFieldControlDirective {
-  @Input('iccFieldControl') fieldControl!: FormControl;
+  @Input('iccFormFieldControl') fieldControl!: FormControl;
 }
