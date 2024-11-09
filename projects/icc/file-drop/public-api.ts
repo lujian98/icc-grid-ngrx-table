217f71c6b8e09bdec80982733e57c729fd90199c
Jimmy Lu <lujian98@yahoo.com>
2024-11-09 16:49:49 -0600

    add file drop

--- projects/icc/file-drop/public-api.ts
+++ projects/icc/file-drop/public-api.ts
@@ -1,3 +1,4 @@
+// from https://github.com/georgipeltekov/ngx-file-drop
 export { IccFileDropComponent } from './src/file-drop.component';
 export { IccFileDropEntry } from './src/file-drop-entry';
 export { FileSystemEntry, FileSystemDirectoryEntry, FileSystemFileEntry } from './src/dom.types';
