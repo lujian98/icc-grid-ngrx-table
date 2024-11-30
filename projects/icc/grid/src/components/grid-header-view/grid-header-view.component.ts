a034b5e444fd23376a96e2371bd739e079b630ef
Jimmy Lu <lujian98@yahoo.com>
2024-11-29 20:08:58 -0600

    add grid header view

---
+++ projects/icc/grid/src/components/grid-header-view/grid-header-view.component.ts
@@ -0,0 +1,115 @@
+import { DragDropModule, moveItemInArray } from '@angular/cdk/drag-drop';
+import { ScrollingModule } from '@angular/cdk/scrolling';
+import { CommonModule } from '@angular/common';
+import { ChangeDetectionStrategy, Component, EventEmitter, inject, Input, Output } from '@angular/core';
+import { IccGridFacade } from '../../+state/grid.facade';
+import { ROW_SELECTION_CELL_WIDTH } from '../../models/constants';
+import { DragDropEvent } from '../../models/drag-drop-event';
+import { IccColumnConfig, IccColumnWidth, IccGridConfig } from '../../models/grid-column.model';
+import { getTableWidth, viewportWidthRatio } from '../../utils/viewport-width-ratio';
+import { IccGridHeaderComponent } from '../grid-header/grid-header.component';
+import { IccGridRowComponent } from '../grid-row/grid-row.component';
+
+@Component({
+  selector: 'icc-grid-header-view',
+  templateUrl: './grid-header-view.component.html',
+  changeDetection: ChangeDetectionStrategy.OnPush,
+  standalone: true,
+  imports: [CommonModule, DragDropModule, ScrollingModule, IccGridHeaderComponent, IccGridRowComponent],
+})
+export class IccGridHeaderViewComponent<T> {
+  private gridFacade = inject(IccGridFacade);
+  private _gridConfig!: IccGridConfig;
+  private _columns: IccColumnConfig[] = [];
+  private _gridTemplateColumns: string = '';
+  private _columnWidths: IccColumnWidth[] = [];
+  tableWidth: number = 2000;
+
+  @Input() columnHeaderPosition: number = 0;
+
+  @Input()
+  set columns(val: IccColumnConfig[]) {
+    this._columns = [...val];
+    const widthRatio = viewportWidthRatio(this.gridConfig, this.columns);
+    this.setColumWidths(this.columns, widthRatio);
+  }
+  get columns(): IccColumnConfig[] {
+    return this._columns;
+  }
+
+  @Input()
+  set gridConfig(val: IccGridConfig) {
+    this._gridConfig = { ...val };
+    const widthRatio = viewportWidthRatio(this.gridConfig, this.columns);
+    this.setColumWidths(this.columns, widthRatio);
+  }
+  get gridConfig(): IccGridConfig {
+    return this._gridConfig;
+  }
+
+  set columnWidths(values: IccColumnWidth[]) {
+    this._columnWidths = values;
+  }
+
+  get columnWidths(): IccColumnWidth[] {
+    return this._columnWidths;
+  }
+
+  set gridTemplateColumns(value: string) {
+    this._gridTemplateColumns = value;
+    this.gridTemplateColumnsEvent.emit(value);
+  }
+
+  get gridTemplateColumns(): string {
+    return this._gridTemplateColumns;
+  }
+
+  @Output() gridTemplateColumnsEvent = new EventEmitter<string>();
+
+  onColumnResizing(columnWidths: IccColumnWidth[]): void {
+    this.setColumWidths(columnWidths, 1);
+    if (this.gridConfig.horizontalScroll) {
+      this.tableWidth = getTableWidth(columnWidths);
+    }
+  }
+
+  onColumnResized(columnWidths: IccColumnWidth[]): void {
+    const widthRatio = viewportWidthRatio(this.gridConfig, this.columns);
+    const columns: IccColumnConfig[] = [...this.columns].map((column, index) => ({
+      ...column,
+      width: columnWidths[index].width / widthRatio,
+    }));
+    this.setColumWidths(columnWidths, widthRatio);
+    this.gridFacade.setGridColumnsConfig(this.gridConfig, columns);
+  }
+
+  onColumnDragDrop(events: DragDropEvent): void {
+    const previousIndex = this.indexCorrection(events.previousIndex);
+    const currentIndex = this.indexCorrection(events.currentIndex);
+    const columns = [...this.columns];
+    moveItemInArray(columns, previousIndex, currentIndex);
+    this.gridFacade.setGridColumnsConfig(this.gridConfig, columns);
+  }
+
+  private setColumWidths(columns: any[], widthRatio: number): void {
+    if (this.gridConfig.horizontalScroll) {
+      this.tableWidth = getTableWidth(this.columns);
+    } else {
+      this.tableWidth = this.gridConfig.viewportWidth;
+    }
+    const colWidths = [...columns]
+      .filter((column) => column.hidden !== true)
+      .map((column) => widthRatio * column.width! + 'px')
+      .join(' ');
+    this.gridTemplateColumns = this.gridConfig.rowSelection ? `${ROW_SELECTION_CELL_WIDTH}px ${colWidths}` : colWidths;
+  }
+
+  private indexCorrection(idx: number): number {
+    this.columns.forEach((column, index) => {
+      if (column.hidden && idx >= index) {
+        idx++;
+      }
+    });
+    return idx;
+  }
+}
--- projects/icc/grid/src/components/grid-view.component.ts
+++ projects/icc/grid/src/components/grid-view.component.ts
@@ -1,4 +1,4 @@
-import { DragDropModule, moveItemInArray } from '@angular/cdk/drag-drop';
+import { DragDropModule } from '@angular/cdk/drag-drop';
 import { CdkVirtualScrollViewport, ScrollingModule } from '@angular/cdk/scrolling';
 import { CommonModule } from '@angular/common';
 import {
@@ -14,10 +14,8 @@ import {
 import { BehaviorSubject, Observable, interval, of } from 'rxjs';
 import { debounceTime, distinctUntilChanged, skip, switchMap, take, takeUntil } from 'rxjs/operators';
 import { IccGridFacade } from '../+state/grid.facade';
-import { ROW_SELECTION_CELL_WIDTH } from '../models/constants';
-import { DragDropEvent } from '../models/drag-drop-event';
-import { IccColumnConfig, IccColumnWidth, IccGridConfig } from '../models/grid-column.model';
-import { getTableWidth, viewportWidthRatio } from '../utils/viewport-width-ratio';
+import { IccColumnConfig, IccGridConfig } from '../models/grid-column.model';
+import { IccGridHeaderViewComponent } from './grid-header-view/grid-header-view.component';
 import { IccGridHeaderComponent } from './grid-header/grid-header.component';
 import { IccGridRowComponent } from './grid-row/grid-row.component';
 
@@ -27,54 +25,36 @@ import { IccGridRowComponent } from './grid-row/grid-row.component';
   styleUrls: ['./grid-view.component.scss'],
   changeDetection: ChangeDetectionStrategy.OnPush,
   standalone: true,
-  imports: [CommonModule, DragDropModule, ScrollingModule, IccGridHeaderComponent, IccGridRowComponent],
+  imports: [
+    CommonModule,
+    DragDropModule,
+    ScrollingModule,
+    IccGridHeaderComponent,
+    IccGridRowComponent,
+    IccGridHeaderViewComponent,
+  ],
 })
 export class IccGridViewComponent<T> implements AfterViewInit, OnDestroy {
   private gridFacade = inject(IccGridFacade);
   private _gridConfig!: IccGridConfig;
-  private _columns: IccColumnConfig[] = [];
-  private _gridTemplateColumns: string = '';
-  private _columnWidths: IccColumnWidth[] = [];
+  gridTemplateColumns: string = '';
   sizeChanged$: BehaviorSubject<any> = new BehaviorSubject({});
   gridData$!: Observable<T[]>;
   columnHeaderPosition = 0;
-  tableWidth: number = 2000;
 
-  @Input()
-  set columns(val: IccColumnConfig[]) {
-    this._columns = [...val];
-    const widthRatio = viewportWidthRatio(this.gridConfig, this.columns);
-    this.setColumWidths(this.columns, widthRatio);
-  }
-  get columns(): IccColumnConfig[] {
-    return this._columns;
-  }
+  @Input() columns: IccColumnConfig[] = [];
 
   @Input()
   set gridConfig(val: IccGridConfig) {
     this._gridConfig = { ...val };
     this.gridData$ = this.gridFacade.selectGridData(this.gridConfig);
-    const widthRatio = viewportWidthRatio(this.gridConfig, this.columns);
-    this.setColumWidths(this.columns, widthRatio);
   }
   get gridConfig(): IccGridConfig {
     return this._gridConfig;
   }
 
-  set columnWidths(values: IccColumnWidth[]) {
-    this._columnWidths = values;
-  }
-
-  get columnWidths(): IccColumnWidth[] {
-    return this._columnWidths;
-  }
-
-  set gridTemplateColumns(value: string) {
-    this._gridTemplateColumns = value;
-  }
-
-  get gridTemplateColumns(): string {
-    return this._gridTemplateColumns;
+  gridTemplateColumnsEvent(event: string): void {
+    this.gridTemplateColumns = event;
   }
 
   @ViewChild(CdkVirtualScrollViewport, { static: true })
@@ -99,31 +79,6 @@ export class IccGridViewComponent<T> implements AfterViewInit, OnDestroy {
     return index;
   }
 
-  onColumnResizing(columnWidths: IccColumnWidth[]): void {
-    this.setColumWidths(columnWidths, 1);
-    if (this.gridConfig.horizontalScroll) {
-      this.tableWidth = getTableWidth(columnWidths);
-    }
-  }
-
-  onColumnResized(columnWidths: IccColumnWidth[]): void {
-    const widthRatio = viewportWidthRatio(this.gridConfig, this.columns);
-    const columns: IccColumnConfig[] = [...this.columns].map((column, index) => ({
-      ...column,
-      width: columnWidths[index].width / widthRatio,
-    }));
-    this.setColumWidths(columnWidths, widthRatio);
-    this.gridFacade.setGridColumnsConfig(this.gridConfig, columns);
-  }
-
-  onColumnDragDrop(events: DragDropEvent): void {
-    const previousIndex = this.indexCorrection(events.previousIndex);
-    const currentIndex = this.indexCorrection(events.currentIndex);
-    const columns = [...this.columns];
-    moveItemInArray(columns, previousIndex, currentIndex);
-    this.gridFacade.setGridColumnsConfig(this.gridConfig, columns);
-  }
-
   onScrolledIndexChange(index: number): void {
     if (this.gridConfig.virtualScroll) {
       const nextPage = this.gridConfig.page + 1;
@@ -137,28 +92,6 @@ export class IccGridViewComponent<T> implements AfterViewInit, OnDestroy {
 
   onViewportScroll(event: any): void {
     this.columnHeaderPosition = -event.target.scrollLeft;
-  }
-
-  private setColumWidths(columns: any[], widthRatio: number): void {
-    if (this.gridConfig.horizontalScroll) {
-      this.tableWidth = getTableWidth(this.columns);
-    } else {
-      this.tableWidth = this.gridConfig.viewportWidth;
-    }
-    const colWidths = [...columns]
-      .filter((column) => column.hidden !== true)
-      .map((column) => widthRatio * column.width! + 'px')
-      .join(' ');
-    this.gridTemplateColumns = this.gridConfig.rowSelection ? `${ROW_SELECTION_CELL_WIDTH}px ${colWidths}` : colWidths;
-  }
-
-  private indexCorrection(idx: number): number {
-    this.columns.forEach((column, index) => {
-      if (column.hidden && idx >= index) {
-        idx++;
-      }
-    });
-    return idx;
   }
 
   private setViewportPageSize(): void {
