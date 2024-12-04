b031340a9affbc32be3ebbed1a9350ff48abf2b2
Jimmy Lu <lujian98@yahoo.com>
2024-12-04 16:41:11 -0600

    layout height

--- projects/icc-demo/src/app/views/dashboard/demos/dashboard-demo/dashboard-demo.component.ts
+++ projects/icc-demo/src/app/views/dashboard/demos/dashboard-demo/dashboard-demo.component.ts
@@ -4,6 +4,9 @@ import { Tile, getColor, IccDashboardComponent } from '@icc/ui/dashboard';
 
 import { PortalDemoComponent } from '../portal-demo/portal-demo.component';
 import { PortalDemo2Component } from '../portal-demo2/portal-demo2.component';
+import { AppStockChartComponent } from '../../../d3/demos/stock-charts/stock-chart.component';
+//import { AppDefaultGridComponent } from '../../../grid/in-memory-data/default-grid.component';
+import { AppGridRemoteVirtualScrollComponent } from '../../../grid/remote-data/grid-virtual-scroll.component';
 
 @Component({
   selector: 'app-dashboard-demo',
@@ -29,35 +32,52 @@ export class AppDashboardDemoComponent {
       {
         name: 'Card 1',
         title: 'test 987',
-        rowStart: 1,
-        rowHeight: 1,
-        colStart: 1,
-        colWidth: 4,
-        color: getColor(),
-        content: PortalDemo2Component,
+        rowStart: 4,
+        rowHeight: 3,
+        colStart: 2,
+        colWidth: 6,
+        color: '#e8eaed',
+        //color: getColor(),
+        content: AppStockChartComponent,
         context: this.portalData,
       },
       {
         name: 'Card 2',
-        rowStart: 2,
+        rowStart: 1,
         rowHeight: 2,
         colStart: 1,
         colWidth: 2,
         color: getColor(),
         content: PortalDemoComponent,
-        context: this.portalData2,
+        context: this.portalData,
       },
-      { name: 'Card 3', rowStart: 2, rowHeight: 1, colStart: 3, colWidth: 1, color: getColor() },
-      { name: 'Card 4', rowStart: 3, rowHeight: 1, colStart: 3, colWidth: 1, color: getColor() },
       {
-        name: 'Card 5',
-        rowStart: 3,
+        name: 'Card 3',
+        rowStart: 1,
         rowHeight: 1,
-        colStart: 5,
-        colWidth: 2,
+        colStart: 3,
+        colWidth: 1,
+        color: getColor(),
+        content: PortalDemo2Component,
+      },
+      {
+        name: 'Card 4',
+        rowStart: 2,
+        rowHeight: 1,
+        colStart: 3,
+        colWidth: 1,
         color: getColor(),
         content: PortalDemoComponent,
-        context: this.portalData,
+        context: this.portalData2,
+      },
+      {
+        name: 'Card 5',
+        rowStart: 1,
+        rowHeight: 3,
+        colStart: 4,
+        colWidth: 6,
+        content: AppGridRemoteVirtualScrollComponent,
+        //color: getColor(),
       },
     ];
 
