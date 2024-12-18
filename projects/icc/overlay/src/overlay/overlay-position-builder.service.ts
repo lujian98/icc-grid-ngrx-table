72a067975d722a5a757e7a52c9437d16a4de3f8d
Jimmy Lu <lujian98@yahoo.com>
2024-12-18 12:05:33 -0600

    fix dialog

---
+++ projects/icc/overlay/src/overlay/overlay-position-builder.service.ts
@@ -0,0 +1,106 @@
+import { Injectable, Inject, ElementRef } from '@angular/core';
+import {
+  OverlayPositionBuilder,
+  GlobalPositionStrategy,
+  FlexibleConnectedPositionStrategy,
+  ConnectionPositionPair,
+} from '@angular/cdk/overlay';
+
+import { IccPosition } from './overlay-position';
+import { ICC_DOCUMENT } from '../document';
+
+export interface Point {
+  x: number;
+  y: number;
+}
+
+const POSITIONS = {
+  [IccPosition.TOP](offset: number): ConnectionPositionPair {
+    return {
+      originX: 'center',
+      originY: 'top',
+      overlayX: 'center',
+      overlayY: 'bottom',
+      offsetY: -offset,
+    };
+  },
+  [IccPosition.BOTTOM](offset: number): ConnectionPositionPair {
+    return {
+      originX: 'center',
+      originY: 'bottom',
+      overlayX: 'center',
+      overlayY: 'top',
+      offsetY: offset,
+    };
+  },
+  [IccPosition.LEFT](offset: number): ConnectionPositionPair {
+    return {
+      originX: 'start',
+      originY: 'center',
+      overlayX: 'end',
+      overlayY: 'center',
+      offsetX: -offset,
+    };
+  },
+  [IccPosition.RIGHT](offset: number): ConnectionPositionPair {
+    return {
+      originX: 'end',
+      originY: 'center',
+      overlayX: 'start',
+      overlayY: 'center',
+      offsetX: offset,
+    };
+  },
+  [IccPosition.BOTTOM_END](offset: number): ConnectionPositionPair {
+    return {
+      originX: 'start',
+      originY: 'bottom',
+      overlayX: 'start',
+      overlayY: 'top',
+      offsetY: offset,
+    };
+  },
+  [IccPosition.BOTTOMRIGHT](offset: number): ConnectionPositionPair {
+    return {
+      originX: 'end',
+      originY: 'bottom',
+      overlayX: 'start',
+      overlayY: 'top',
+      offsetX: offset,
+      offsetY: offset - 6,
+    };
+  },
+  [IccPosition.RIGHTBOTTOM](offset: number): ConnectionPositionPair {
+    return {
+      originX: 'end',
+      originY: 'bottom',
+      overlayX: 'start',
+      overlayY: 'top',
+      offsetX: offset - 10,
+      offsetY: -3 * offset,
+    };
+  },
+};
+
+@Injectable()
+export class IccPositionBuilderService {
+  constructor(
+    @Inject(ICC_DOCUMENT) protected document: Document,
+    protected overlayPositionBuilder: OverlayPositionBuilder,
+  ) {}
+
+  global(): GlobalPositionStrategy {
+    return new GlobalPositionStrategy();
+  }
+
+  flexibleConnectedTo(
+    elementRef: ElementRef | Point,
+    position: IccPosition,
+    offset: number = 8,
+  ): FlexibleConnectedPositionStrategy {
+    const connectedPosition = POSITIONS[position](offset);
+    const positions = [];
+    positions.push(connectedPosition);
+    return this.overlayPositionBuilder.flexibleConnectedTo(elementRef).withPositions(positions);
+  }
+}
---
+++ projects/icc/overlay/src/overlay/overlay-position.ts
@@ -0,0 +1,9 @@
+export enum IccPosition {
+  TOP = 'top',
+  BOTTOM = 'bottom',
+  LEFT = 'left',
+  RIGHT = 'right',
+  BOTTOM_END = 'bottom-end',
+  BOTTOMRIGHT = 'bottomRight',
+  RIGHTBOTTOM = 'rightBottom',
+}
