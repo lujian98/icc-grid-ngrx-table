c987fec1f36eaa9338670d17656e12e5687db01e
Jimmy Lu <lujian98@yahoo.com>
2024-12-18 14:04:49 -0600

    dialog resize

--- projects/icc-demo/src/app/views/menu/demos/simple-menu/dialog-test.component.ts
+++ projects/icc-demo/src/app/views/menu/demos/simple-menu/dialog-test.component.ts
@@ -1,24 +1,16 @@
 import { CommonModule } from '@angular/common';
-import { ChangeDetectionStrategy, Component, inject, ElementRef, Input } from '@angular/core';
-import { ReactiveFormsModule } from '@angular/forms';
-import { IccButtonComponent } from '@icc/ui/button';
-import { IccCalendarModule } from '@icc/ui/calendar';
-import { IccCheckboxComponent } from '@icc/ui/checkbox';
-import { IccFormComponent } from '@icc/ui/form';
-import { IccFormFieldComponent, IccLabelDirective, IccSuffixDirective } from '@icc/ui/form-field';
+import { ChangeDetectionStrategy, Component, inject } from '@angular/core';
 import {
-  IccLayoutComponent,
-  IccLayoutHeaderComponent,
-  IccLayoutFooterComponent,
   IccLayoutCenterComponent,
+  IccLayoutComponent,
+  IccLayoutFooterComponent,
+  IccLayoutHeaderComponent,
   IccLayoutHorizontalComponent,
   IccLayoutLeftComponent,
   IccLayoutRightComponent,
 } from '@icc/ui/layout';
-import { uniqueId } from '@icc/ui/core';
-import { IccResizeDirective, IccResizeInfo, IccResizeType } from '@icc/ui/resize';
+import { IccDialogRef } from '@icc/ui/overlay';
 import { AppDialogDemoComponent } from './dialog.component';
-import { IccDialogRef, IccDynamicOverlayService, IccOverlayModule } from '@icc/ui/overlay';
 
 @Component({
   selector: 'app-dialog-test',
@@ -28,8 +20,6 @@ import { IccDialogRef, IccDynamicOverlayService, IccOverlayModule } from '@icc/u
   standalone: true,
   imports: [
     CommonModule,
-    IccButtonComponent,
-    IccOverlayModule,
     IccLayoutComponent,
     IccLayoutHeaderComponent,
     IccLayoutFooterComponent,
@@ -37,56 +27,15 @@ import { IccDialogRef, IccDynamicOverlayService, IccOverlayModule } from '@icc/u
     IccLayoutHorizontalComponent,
     IccLayoutLeftComponent,
     IccLayoutRightComponent,
-    IccFormComponent,
-    ReactiveFormsModule,
     AppDialogDemoComponent,
-    IccCheckboxComponent,
-    IccCalendarModule,
-
-    IccFormFieldComponent,
-    IccLabelDirective,
-    IccSuffixDirective,
-    IccResizeDirective,
   ],
-  providers: [IccDynamicOverlayService],
 })
 export class AppDialogTestDemoComponent {
-  private elementRef = inject(ElementRef);
-  resizeType = IccResizeType;
-  elementKey = uniqueId(16);
-  @Input() resizeable: boolean = true;
-
-  onResizePanel(resizeInfo: IccResizeInfo): void {
-    if (resizeInfo.isResized) {
-      const height = resizeInfo.height * resizeInfo.scaleY;
-      const width = resizeInfo.width * resizeInfo.scaleX;
-      this.setHeight(`${height}px`);
-      this.setWidth(`${width}px`);
-    }
-  }
-
-  private setHeight(height: string): void {
-    const el = this.elementRef.nativeElement;
-    if (el) {
-      el.style.height = height;
-    }
-  }
-
-  private setWidth(width: string): void {
-    const el = this.elementRef.nativeElement;
-    if (el) {
-      el.style.width = width;
-      //el.parentNode.style.width = width;
-    }
-  }
-
   private dialogRef = inject(IccDialogRef<AppDialogTestDemoComponent>);
 
   dialog: any;
 
   close(): void {
-    //console.log('tttttttttttttttttttt close clicked');
-    // this.dynamicOverlayService.hide();
     this.dialogRef.close('test uujj make love');
   }
 }
