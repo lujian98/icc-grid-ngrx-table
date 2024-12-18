c0d8d2b8b37942d792f5d32bc08593644640ae56
Jimmy Lu <lujian98@yahoo.com>
2024-12-18 13:42:36 -0600

    dialog resize

--- projects/icc-demo/src/app/views/menu/demos/simple-menu/dialog.component.ts
+++ projects/icc-demo/src/app/views/menu/demos/simple-menu/dialog.component.ts
@@ -1,11 +1,22 @@
 import { CommonModule } from '@angular/common';
-import { ChangeDetectionStrategy, Component, inject } from '@angular/core';
+import { ChangeDetectionStrategy, Component, inject, ElementRef, Input } from '@angular/core';
 import { ReactiveFormsModule } from '@angular/forms';
 import { IccButtonComponent } from '@icc/ui/button';
 import { IccCalendarModule } from '@icc/ui/calendar';
 import { IccCheckboxComponent } from '@icc/ui/checkbox';
 import { IccFormComponent } from '@icc/ui/form';
 import { IccFormFieldComponent, IccLabelDirective, IccSuffixDirective } from '@icc/ui/form-field';
+import {
+  IccLayoutComponent,
+  IccLayoutHeaderComponent,
+  IccLayoutFooterComponent,
+  IccLayoutCenterComponent,
+  IccLayoutHorizontalComponent,
+  IccLayoutLeftComponent,
+  IccLayoutRightComponent,
+} from '@icc/ui/layout';
+import { uniqueId } from '@icc/ui/core';
+import { IccResizeDirective, IccResizeInfo, IccResizeType } from '@icc/ui/resize';
 
 import { IccDialogRef, IccDynamicOverlayService, IccOverlayModule } from '@icc/ui/overlay';
 
@@ -19,6 +30,13 @@ import { IccDialogRef, IccDynamicOverlayService, IccOverlayModule } from '@icc/u
     CommonModule,
     IccButtonComponent,
     IccOverlayModule,
+    IccLayoutComponent,
+    IccLayoutHeaderComponent,
+    IccLayoutFooterComponent,
+    IccLayoutCenterComponent,
+    IccLayoutHorizontalComponent,
+    IccLayoutLeftComponent,
+    IccLayoutRightComponent,
     IccFormComponent,
     ReactiveFormsModule,
 
@@ -28,10 +46,40 @@ import { IccDialogRef, IccDynamicOverlayService, IccOverlayModule } from '@icc/u
     IccFormFieldComponent,
     IccLabelDirective,
     IccSuffixDirective,
+    IccResizeDirective,
   ],
   providers: [IccDynamicOverlayService],
 })
 export class AppDialogDemoComponent {
+  private elementRef = inject(ElementRef);
+  resizeType = IccResizeType;
+  elementKey = uniqueId(16);
+  @Input() resizeable: boolean = true;
+
+  onResizePanel(resizeInfo: IccResizeInfo): void {
+    if (resizeInfo.isResized) {
+      const height = resizeInfo.height * resizeInfo.scaleY;
+      const width = resizeInfo.width * resizeInfo.scaleX;
+      this.setHeight(`${height}px`);
+      this.setWidth(`${width}px`);
+    }
+  }
+
+  private setHeight(height: string): void {
+    const el = this.elementRef.nativeElement;
+    if (el) {
+      el.style.height = height;
+    }
+  }
+
+  private setWidth(width: string): void {
+    const el = this.elementRef.nativeElement;
+    if (el) {
+      el.style.width = width;
+      //el.parentNode.style.width = width;
+    }
+  }
+
   private dialogRef = inject(IccDialogRef<AppDialogDemoComponent>);
 
   dialog: any;
