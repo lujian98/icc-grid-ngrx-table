00e0292323fb8b15adc3a1d1de3d3d73b527a796
Jimmy Lu <lujian98@yahoo.com>
2025-02-14 09:08:54 -0600

    tab close

--- projects/icc/tabs/src/tabs.component.ts
+++ projects/icc/tabs/src/tabs.component.ts
@@ -1,7 +1,10 @@
 import { CdkDrag, CdkDragDrop, CdkDropList, DragDropModule, moveItemInArray } from '@angular/cdk/drag-drop';
 import { CommonModule } from '@angular/common';
-import { ChangeDetectionStrategy, Component, Input } from '@angular/core';
+import { ChangeDetectionStrategy, ChangeDetectorRef, Component, inject, Input } from '@angular/core';
+import { IccButtonComponent } from '@icc/ui/button';
+import { IccIconModule } from '@icc/ui/icon';
 import { IccPortalComponent } from '@icc/ui/portal';
+import { take, timer } from 'rxjs';
 import { IccTabGroupComponent } from './components/tab-group/tab-group.component';
 import { IccTabComponent } from './components/tab/tab.component';
 import { IccTabLabelDirective } from './directives/tab-label.directive';
@@ -21,9 +24,12 @@ import { IccTabConfig } from './models/tabs.model';
     IccTabComponent,
     IccTabGroupComponent,
     IccPortalComponent,
+    IccButtonComponent,
+    IccIconModule,
   ],
 })
 export class IccTabsComponent<T> {
+  private changeDetectorRef = inject(ChangeDetectorRef);
   protected selectedTabIndex = 0;
 
   @Input() tabs!: IccTabConfig[];
@@ -40,5 +46,22 @@ export class IccTabsComponent<T> {
     const prevActive = this.tabs[this.selectedTabIndex];
     moveItemInArray(this.tabs, event.previousIndex, event.currentIndex);
     this.selectedTabIndex = this.tabs.indexOf(prevActive);
+  }
+
+  removeTab(tab: IccTabConfig, index: number): void {
+    this.tabs = [...this.tabs].filter((item) => item.name !== tab.name);
+    if (index === this.selectedTabIndex) {
+      if (this.selectedTabIndex === 0) {
+        this.selectedTabIndex = -1;
+        timer(10)
+          .pipe(take(1))
+          .subscribe(() => {
+            this.selectedTabIndex = 0;
+            this.changeDetectorRef.markForCheck();
+          });
+      } else {
+        this.selectedTabIndex = 0;
+      }
+    }
   }
 }
