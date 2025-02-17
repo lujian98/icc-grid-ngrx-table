13660b3ff7df38967bec420e57897666550cdacf
Jimmy Lu <lujian98@yahoo.com>
2025-02-17 07:27:40 -0600

    add tasks services

--- projects/icc/grid/src/models/default-grid.ts
+++ projects/icc/grid/src/models/default-grid.ts
@@ -32,7 +32,7 @@ export const defaultGridConfig: IccGridConfig = {
   hideGridFooter: false,
   rowHeight: 24, //px
   headerHeight: 30, //px
-  refreshRate: 10, //seconds  min 5
+  refreshRate: 0, //seconds  min 5 to have refresh the grid data
   lastUpdateTime: new Date(),
 };
 
