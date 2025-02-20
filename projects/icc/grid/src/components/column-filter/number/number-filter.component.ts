e7388642ce50f9397ada444663f1a82410aa489c
Jimmy Lu <lujian98@yahoo.com>
2025-02-20 14:13:42 -0600

    number i18n

--- projects/icc/grid/src/components/column-filter/number/number-filter.component.ts
+++ projects/icc/grid/src/components/column-filter/number/number-filter.component.ts
@@ -1,6 +1,7 @@
 import { CommonModule } from '@angular/common';
-import { ChangeDetectionStrategy, Component, inject } from '@angular/core';
+import { ChangeDetectionStrategy, Component, inject, OnInit, OnDestroy } from '@angular/core';
 import { IccTextFieldComponent, IccTextFieldConfig } from '@icc/ui/fields';
+import { delay, Subject, takeUntil } from 'rxjs';
 import { TranslateService } from '@ngx-translate/core';
 import { IccFieldFilterComponent } from '../field-filter.component';
 
@@ -11,9 +12,10 @@ import { IccFieldFilterComponent } from '../field-filter.component';
   changeDetection: ChangeDetectionStrategy.OnPush,
   imports: [CommonModule, IccTextFieldComponent],
 })
-export class IccNumberFilterComponent extends IccFieldFilterComponent {
+export class IccNumberFilterComponent extends IccFieldFilterComponent implements OnInit, OnDestroy {
   override fieldConfig!: Partial<IccTextFieldConfig>;
   private translateService = inject(TranslateService);
+  private destroy$ = new Subject<void>();
 
   override checkField(): void {
     const filterI18n = this.translateService.instant('ICC.UI.GRID.FILTER');
@@ -33,7 +35,20 @@ export class IccNumberFilterComponent extends IccFieldFilterComponent {
     return this._value as string;
   }
 
+  ngOnInit(): void {
+    this.translateService.onLangChange.pipe(delay(50), takeUntil(this.destroy$)).subscribe(() => {
+      this.checkField();
+      this.changeDetectorRef.markForCheck();
+    });
+  }
+
   onValueChange(value: string): void {
     this.filterChanged$.next(value);
+  }
+
+  override ngOnDestroy(): void {
+    super.ngOnDestroy();
+    this.destroy$.next();
+    this.destroy$.complete();
   }
 }
