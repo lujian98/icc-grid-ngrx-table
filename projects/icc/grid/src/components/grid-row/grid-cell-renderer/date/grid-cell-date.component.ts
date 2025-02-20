4b7c19ad776e8a69fba8d141eb26ce9cd39d73c4
Jimmy Lu <lujian98@yahoo.com>
2025-02-20 12:49:08 -0600

    date lang change

--- projects/icc/grid/src/components/grid-row/grid-cell-renderer/date/grid-cell-date.component.ts
+++ projects/icc/grid/src/components/grid-row/grid-cell-renderer/date/grid-cell-date.component.ts
@@ -1,6 +1,7 @@
 import { CommonModule, DatePipe } from '@angular/common';
-import { ChangeDetectionStrategy, Component, inject } from '@angular/core';
+import { ChangeDetectionStrategy, Component, inject, OnDestroy, OnInit } from '@angular/core';
 import { TranslateService } from '@ngx-translate/core';
+import { delay, Subject, takeUntil } from 'rxjs';
 import { IccGridCellRendererComponent } from '../grid-cell-renderer.component';
 
 @Component({
@@ -10,9 +11,9 @@ import { IccGridCellRendererComponent } from '../grid-cell-renderer.component';
   changeDetection: ChangeDetectionStrategy.OnPush,
   imports: [CommonModule],
 })
-export class IccGridCellDateComponent extends IccGridCellRendererComponent<Date> {
+export class IccGridCellDateComponent extends IccGridCellRendererComponent<Date> implements OnInit, OnDestroy {
   private translateService = inject(TranslateService);
-
+  private destroy$ = new Subject<void>();
   get display(): string {
     if (this.data) {
       const locale = this.translateService.currentLang || 'en-US';
@@ -21,5 +22,16 @@ export class IccGridCellDateComponent extends IccGridCellRendererComponent<Date>
     } else {
       return '';
     }
+  }
+
+  ngOnInit(): void {
+    this.translateService.onLangChange.pipe(delay(50), takeUntil(this.destroy$)).subscribe((lang) => {
+      this.changeDetectorRef.markForCheck();
+    });
+  }
+
+  ngOnDestroy(): void {
+    this.destroy$.next();
+    this.destroy$.complete();
   }
 }
