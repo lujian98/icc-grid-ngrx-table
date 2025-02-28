c9e54862fd3e13a0c36d2082212928a54e89e892
Jimmy Lu <lujian98@yahoo.com>
2025-02-28 12:33:03 -0600

    use menu-item-config

---
+++ projects/icc-demo/src/app/views/menu/demos/mock-menu.ts
@@ -0,0 +1,79 @@
+import { IccMenuConfig } from '@icc/ui/menu';
+
+export const MockMenuItems: IccMenuConfig = {
+  icon: 'ellipsis-v',
+  name: 'group0',
+  children: [
+    {
+      title: 'Speakers',
+      name: 'group',
+      children: [
+        {
+          title: 'Michael Prentice',
+          name: 'person',
+          children: [
+            {
+              title: 'Delight your Organization',
+              name: 'star_rate',
+              //type: 'checkbox',
+            },
+          ],
+        },
+        {
+          title: 'Stephen Fluin',
+          name: 'person',
+          children: [
+            {
+              title: "What's up with the Web?",
+              name: 'star_rate',
+            },
+          ],
+        },
+        {
+          title: 'Mike Brocchi',
+          name: 'person',
+          children: [
+            {
+              title: 'My ally, the CLI',
+              name: 'star_rate',
+            },
+            {
+              title: 'Become an Angular Tailor',
+              name: 'star_rate',
+            },
+          ],
+        },
+        {
+          title: 'James Brocchi',
+          name: 'person',
+        },
+      ],
+    },
+    {
+      title: 'Sessions',
+      name: 'speaker_notes',
+      children: [
+        {
+          title: 'Delight your Organization',
+          name: 'star_rate',
+        },
+        {
+          title: "What's up with the Web?",
+          name: 'star_rate',
+        },
+        {
+          title: 'My ally, the CLI',
+          name: 'star_rate',
+        },
+        {
+          title: 'Become an Angular Tailor',
+          name: 'star_rate',
+        },
+      ],
+    },
+    {
+      title: 'Feedback',
+      name: 'feedback',
+    },
+  ],
+};
--- projects/icc-demo/src/app/views/menu/demos/simple-menu/simple-menu.component.ts
+++ projects/icc-demo/src/app/views/menu/demos/simple-menu/simple-menu.component.ts
@@ -2,6 +2,7 @@ import { CommonModule } from '@angular/common';
 import { ChangeDetectionStrategy, Component, OnInit } from '@angular/core';
 import { IccMenuConfig, IccMenusComponent, IccPopoverMenuComponent } from '@icc/ui/menu';
 import { IccTrigger } from '@icc/ui/overlay';
+import { MockMenuItems } from '../mock-menu';
 
 @Component({
   selector: 'app-simple-menu',
@@ -14,83 +15,7 @@ export class AppSimpleMenuComponent implements OnInit {
   contextmenu: IccTrigger = IccTrigger.CONTEXTMENU;
 
   menuItems: any;
-  testMenuItems: IccMenuConfig = {
-    icon: 'ellipsis-v',
-    name: 'group0',
-    children: [
-      {
-        title: 'Speakers',
-        name: 'group',
-        children: [
-          {
-            title: 'Michael Prentice',
-            name: 'person',
-            children: [
-              {
-                title: 'Delight your Organization',
-                name: 'star_rate',
-                //type: 'checkbox',
-              },
-            ],
-          },
-          {
-            title: 'Stephen Fluin',
-            name: 'person',
-            children: [
-              {
-                title: "What's up with the Web?",
-                name: 'star_rate',
-              },
-            ],
-          },
-          {
-            title: 'Mike Brocchi',
-            name: 'person',
-            children: [
-              {
-                title: 'My ally, the CLI',
-                name: 'star_rate',
-              },
-              {
-                title: 'Become an Angular Tailor',
-                name: 'star_rate',
-              },
-            ],
-          },
-          {
-            title: 'James Brocchi',
-            name: 'person',
-          },
-        ],
-      },
-      {
-        title: 'Sessions',
-        name: 'speaker_notes',
-        children: [
-          {
-            title: 'Delight your Organization',
-            name: 'star_rate',
-          },
-          {
-            title: "What's up with the Web?",
-            name: 'star_rate',
-          },
-          {
-            title: 'My ally, the CLI',
-            name: 'star_rate',
-          },
-          {
-            title: 'Become an Angular Tailor',
-            name: 'star_rate',
-          },
-        ],
-      },
-      {
-        title: 'Feedback',
-        name: 'feedback',
-      },
-    ],
-  };
+  testMenuItems = MockMenuItems;
 
   ngOnInit() {
     this.menuItems = [this.testMenuItems];
