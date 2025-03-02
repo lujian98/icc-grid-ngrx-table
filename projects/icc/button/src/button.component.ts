08f5a8d92d52cf95d5cc3b6dbcdb2386578b5afe
Jimmy Lu <lujian98@yahoo.com>
2025-03-01 20:00:49 -0600

    button

--- projects/icc/button/src/button.component.ts
+++ projects/icc/button/src/button.component.ts
@@ -1,10 +1,7 @@
-import { ChangeDetectionStrategy, Component, Input, ElementRef } from '@angular/core';
+import { ChangeDetectionStrategy, Component, Input, ElementRef, inject } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { coerceBooleanProperty } from '@angular/cdk/coercion';
 
-//import { IccUtils } from '../utils/utils';
-
-//export type IccButtonSize = 'medium' | 'standard';
 export type IccButtonStatus = 'default' | 'primary' | 'danger';
 
 @Component({
@@ -23,30 +20,14 @@ export type IccButtonStatus = 'default' | 'primary' | 'danger';
   imports: [CommonModule],
 })
 export class IccButtonComponent {
-  //@Input() size: IccButtonSize = 'standard';
+  private elementRef = inject(ElementRef<HTMLElement>);
+
   @Input() status: IccButtonStatus = 'default';
   @Input() appearance!: string;
 
   get default() {
     return this.status === 'default';
   }
-  /*
-  get medium() {
-    return this.size === 'medium';
-  }
-
-  get standard() {
-    return this.size === 'standard';
-  }
-
-  get primary() {
-    return this.status === 'primary';
-  }
-
-  @HostBinding('class.status-danger')
-  get danger() {
-    return this.status === 'danger';
-  }*/
 
   @Input()
   get ghost(): boolean {
@@ -57,46 +38,4 @@ export class IccButtonComponent {
       this.appearance = 'ghost';
     }
   }
-
-  /*
- // @HostBinding('class.icon-start')
-  get iconLeft(): boolean {
-    const el = this.elementRef.nativeElement;
-    const icon = this.firstIconElement;
-    const childrenCount = IccUtils.children(el).length;
-    return !!(icon && IccUtils.firstChild(el) === icon && childrenCount > 1);
-  }
-
-  //@HostBinding('class.icon-end')
-  get iconRight(): boolean {
-    const el = this.elementRef.nativeElement;
-    const icon = this.lastIconElement;
-    const iconCount = this.iconElements.length;
-    const childrenCount = IccUtils.children(el).length;
-    return !!(icon && IccUtils.lastChild(el) === icon && childrenCount > iconCount);
-  }*/
-
-  constructor(protected elementRef: ElementRef<HTMLElement>) {}
-
-  /*
-  protected get iconElements() {
-    const el = this.elementRef.nativeElement;
-    return el.querySelectorAll('icc-icon');
-  }
-
-  protected get firstIconElement() {
-    const icons = this.iconElements;
-    if (icons.length > 0) {
-      return icons[0];
-    }
-    return null;
-  }
-
-  protected get lastIconElement() {
-    const icons = this.iconElements;
-    if (icons.length > 0) {
-      return icons[icons.length - 1];
-    }
-    return null;
-  }*/
 }
