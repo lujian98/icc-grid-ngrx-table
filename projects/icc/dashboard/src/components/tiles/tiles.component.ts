0e4ba00e74da1f17a9a0145658f02aff036f0ef5
Jimmy Lu <jimmy.lu@sunbirddcim.com>
2025-06-05 13:31:50 -0500

    accordion input signal

--- projects/icc/dashboard/src/components/tiles/tiles.component.ts
+++ projects/icc/dashboard/src/components/tiles/tiles.component.ts
@@ -1,6 +1,6 @@
 import { CdkDragDrop, CdkDragHandle, DragDropModule } from '@angular/cdk/drag-drop';
 import { CommonModule } from '@angular/common';
-import { ChangeDetectionStrategy, Component, inject, Input, OnInit } from '@angular/core';
+import { ChangeDetectionStrategy, Component, inject, Input, input, OnInit, signal } from '@angular/core';
 import { IccMenuConfig, IccMenusComponent } from '@icc/ui/menu';
 import { IccPosition, IccTrigger } from '@icc/ui/overlay';
 import { IccPopoverDirective } from '@icc/ui/popover';
@@ -40,36 +40,35 @@ import {
   ],
 })
 export class IccTilesComponent implements OnInit {
-  private dashboardFacade = inject(IccDashboardFacade);
-  private _config!: IccDashboardConfig;
+  private readonly dashboardFacade = inject(IccDashboardFacade);
   resizeType = IccResizeType;
   position: IccPosition = IccPosition.BOTTOMRIGHT;
   tileMenus = defaultTileMenus;
 
-  @Input()
-  set config(config: IccDashboardConfig) {
-    if (this.config && (config.rows !== this.config.rows || config.cols !== this.config.cols)) {
-      this.setupTilesLayout(this.tiles); //TODO test confg change
-    }
-    this._config = config;
-  }
-  get config(): IccDashboardConfig {
-    return this._config;
-  }
-  @Input() setting!: IccDashboardSetting;
-  @Input() tiles!: IccTile<unknown>[];
-  @Input() options: IccTileOption<unknown>[] = [];
+  prevConfig = signal<IccDashboardConfig | undefined>(undefined);
+  config = input.required<IccDashboardConfig, IccDashboardConfig>({
+    transform: (config: IccDashboardConfig) => {
+      if (this.prevConfig() && (config.rows !== this.prevConfig()!.rows || config.cols !== this.prevConfig()!.cols)) {
+        this.setupTilesLayout(this.tiles()); //TODO test confg change
+      }
+      this.prevConfig.update(() => config);
+      return config;
+    },
+  });
+  @Input() setting!: IccDashboardSetting; //TODO change to input??? not working
+  tiles = input.required<IccTile<unknown>[]>();
+  options = input<IccTileOption<unknown>[]>([]);
 
   get gridGap(): string {
-    return `${this.config.gridGap}px`;
+    return `${this.config().gridGap}px`;
   }
 
   ngOnInit(): void {
-    this.setupTilesLayout(this.tiles);
+    this.setupTilesLayout(this.tiles());
   }
 
   getPortalContent(tile: IccTile<unknown>): IccPortalContent<unknown> {
-    const find = this.options.find((option) => option.name === tile.portalName);
+    const find = this.options().find((option) => option.name === tile.portalName);
     return find ? find.content : tile.content!;
   }
 
@@ -81,9 +80,9 @@ export class IccTilesComponent implements OnInit {
 
   onResizeTile(resizeInfo: IccResizeInfo, tile: IccTile<unknown>): void {
     if (resizeInfo.isResized) {
-      const tileInfo = tileResizeInfo(resizeInfo, tile, this.config, this.setting.gridMap);
+      const tileInfo = tileResizeInfo(resizeInfo, tile, this.config(), this.setting.gridMap);
       Object.assign(tile, tileInfo);
-      this.setupTilesLayout(this.tiles);
+      this.setupTilesLayout(this.tiles());
     }
   }
 
@@ -92,13 +91,13 @@ export class IccTilesComponent implements OnInit {
   }
 
   onDropListDropped<D>(e: CdkDragDrop<D>, tile: IccTile<unknown>): void {
-    const newTiles = dragDropTile(e, tile, this.tiles, this.config, this.setting.gridMap);
+    const newTiles = dragDropTile(e, tile, this.tiles(), this.config(), this.setting.gridMap);
     this.setupTilesLayout(newTiles);
   }
 
   private setupTilesLayout(tiles: IccTile<unknown>[]): void {
-    const gridMap = initGridMap(this.config); //WARNING initialize gridMap!!!
-    const newTiles = setupTilesLayout(tiles, this.config, gridMap);
+    const gridMap = initGridMap(this.config()); //WARNING initialize gridMap!!!
+    const newTiles = setupTilesLayout(tiles, this.config(), gridMap);
     this.dashboardFacade.loadDashboardGridMapTiles(gridMap, newTiles);
   }
 }
