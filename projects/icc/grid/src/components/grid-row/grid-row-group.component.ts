a9e5bf84eea8a5d7a610a59dc32f7d858a7b76cf
Jimmy Lu <lujian98@yahoo.com>
2025-06-07 22:28:25 -0500

    grid signal

--- projects/icc/grid/src/components/grid-row/grid-row-group.component.ts
+++ projects/icc/grid/src/components/grid-row/grid-row-group.component.ts
@@ -1,20 +1,9 @@
-import { CommonModule } from '@angular/common';
-import {
-  ChangeDetectionStrategy,
-  Component,
-  effect,
-  EventEmitter,
-  signal,
-  Input,
-  input,
-  Output,
-  inject,
-} from '@angular/core';
+import { ChangeDetectionStrategy, Component, effect, EventEmitter, inject, input, Output } from '@angular/core';
 import { IccIconModule } from '@icc/ui/icon';
 import { TranslatePipe } from '@ngx-translate/core';
+import { IccGridFacade } from '../../+state/grid.facade';
 import { IccColumnConfig, IccGridConfig, IccGridSetting } from '../../models/grid-column.model';
 import { IccRowGroup } from '../../utils/row-group/row-group';
-import { IccGridFacade } from '../../+state/grid.facade';
 
 @Component({
   selector: 'icc-grid-row-group',
@@ -24,73 +13,39 @@ import { IccGridFacade } from '../../+state/grid.facade';
   host: {
     '[class.icc-grid-row]': 'iccGridRow',
   },
-  imports: [CommonModule, TranslatePipe, IccIconModule],
+  imports: [TranslatePipe, IccIconModule],
 })
 export class IccGridRowGroupComponent<T> {
   private readonly gridFacade = inject(IccGridFacade);
-  private _record!: IccRowGroup;
-  //record$ = signal<IccRowGroup | undefined>(undefined);
-
   columns = input.required<IccColumnConfig[]>();
   gridSetting = input.required<IccGridSetting>();
   gridConfig = input.required<IccGridConfig>();
   rowIndex = input.required<number>();
-  //record = input.required<IccRowGroup>();
-
-  //@Input() gridSetting!: IccGridSetting;
-  //@Input() gridConfig!: IccGridConfig;
-  //@Input() columns: IccColumnConfig[] = [];
-  //@Input() rowIndex!: number;
-
-  @Input()
-  set record(data: T | IccRowGroup) {
-    if (data instanceof IccRowGroup) {
-      this._record = data;
-    }
-  }
-  get record(): IccRowGroup {
-    return this._record;
-  }
+  record = input.required<T | IccRowGroup>();
+  rowGroup!: IccRowGroup;
 
   get title(): string {
-    const column = this.columns().find((item) => item.name === this.record.field)!;
+    const column = this.columns().find((item) => item.name === this.rowGroup.field)!;
     return column.title || column.name;
   }
 
   @Output() onToggleRowGroup = new EventEmitter<IccRowGroup>();
 
   constructor() {
-    /*
-      effect(() => {
-        if (this.record()) {
-          this.record$.update(()=> {
-            if (this.record() instanceof IccRowGroup) {
-              return this.record();
-            } else {
-              return undefined;
-            }
-          })
-        }
-      });*/
+    effect(() => {
+      if (this.record() instanceof IccRowGroup) {
+        this.rowGroup = this.record() as IccRowGroup;
+      }
+    });
   }
 
   toggleRowGroup(): void {
-    this.record.expanded = !this.record.expanded;
-    /*
-      this.record$.update((record)=> {
-        if (record instanceof IccRowGroup && record !== undefined) {
-          return {...record};
-        } else {
-          return undefined;
-        }
-      });
-      */
-
-    this.gridFacade.setToggleRowGroup(this.gridSetting().gridId, this.record);
-    this.onToggleRowGroup.emit(this.record);
+    this.rowGroup.expanded = !this.rowGroup.expanded;
+    this.gridFacade.setToggleRowGroup(this.gridSetting().gridId, this.rowGroup);
+    this.onToggleRowGroup.emit(this.rowGroup);
   }
 
-  get iccGridRow() {
+  get iccGridRow(): boolean {
     return true;
   }
 }
