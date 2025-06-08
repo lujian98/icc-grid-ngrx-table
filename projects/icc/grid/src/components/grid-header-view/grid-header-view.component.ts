ced076221d0f58b7ad344768dc06b66fe9ffb6c8
Jimmy Lu <lujian98@yahoo.com>
2025-06-08 08:46:32 -0500

    grid signal

--- projects/icc/grid/src/components/grid-header-view/grid-header-view.component.ts
+++ projects/icc/grid/src/components/grid-header-view/grid-header-view.component.ts
@@ -1,14 +1,14 @@
 import { DragDropModule, moveItemInArray } from '@angular/cdk/drag-drop';
 import { ScrollingModule } from '@angular/cdk/scrolling';
 import { CommonModule } from '@angular/common';
-import { ChangeDetectionStrategy, Component, EventEmitter, inject, Input, Output } from '@angular/core';
+import { ChangeDetectionStrategy, Component, effect, EventEmitter, inject, input, Output, signal } from '@angular/core';
 import { IccGridFacade } from '../../+state/grid.facade';
 import { ROW_SELECTION_CELL_WIDTH } from '../../models/constants';
 import { DragDropEvent } from '../../models/drag-drop-event';
 import { IccColumnConfig, IccColumnWidth, IccGridConfig, IccGridSetting } from '../../models/grid-column.model';
 import { getTableWidth, viewportWidthRatio } from '../../utils/viewport-width-ratio';
-import { IccGridHeaderComponent } from '../grid-header/grid-header.component';
 import { IccGridGroupHeaderComponent } from '../grid-header/grid-group-header/grid-group-header.component';
+import { IccGridHeaderComponent } from '../grid-header/grid-header.component';
 
 @Component({
   selector: 'icc-grid-header-view',
@@ -18,34 +18,14 @@ import { IccGridGroupHeaderComponent } from '../grid-header/grid-group-header/gr
   imports: [CommonModule, DragDropModule, ScrollingModule, IccGridHeaderComponent, IccGridGroupHeaderComponent],
 })
 export class IccGridHeaderViewComponent {
-  private gridFacade = inject(IccGridFacade);
-  private _gridConfig!: IccGridConfig;
-  private _columns: IccColumnConfig[] = [];
+  private readonly gridFacade = inject(IccGridFacade);
   private _columnWidths: IccColumnWidth[] = [];
   tableWidth: number = 1000;
-
-  @Input() gridSetting!: IccGridSetting;
-  @Input() columnHeaderPosition: number = 0;
-
-  @Input()
-  set columns(val: IccColumnConfig[]) {
-    this._columns = [...val];
-    const widthRatio = viewportWidthRatio(this.gridConfig, this.gridSetting, this.columns);
-    this.setColumWidths(this.columns, widthRatio);
-  }
-  get columns(): IccColumnConfig[] {
-    return this._columns;
-  }
-
-  @Input()
-  set gridConfig(val: IccGridConfig) {
-    this._gridConfig = { ...val };
-    const widthRatio = viewportWidthRatio(this.gridConfig, this.gridSetting, this.columns);
-    this.setColumWidths(this.columns, widthRatio);
-  }
-  get gridConfig(): IccGridConfig {
-    return this._gridConfig;
-  }
+  gridSetting = input.required<IccGridSetting>();
+  columnHeaderPosition = input<number>(0);
+  gridConfig = input.required<IccGridConfig>();
+  columnConfigs = input.required<IccColumnConfig[]>();
+  columns = signal<IccColumnConfig[]>([]);
 
   set columnWidths(values: IccColumnWidth[]) {
     this._columnWidths = values;
@@ -58,27 +38,37 @@ export class IccGridHeaderViewComponent {
 
   @Output() gridColumnWidthsEvent = new EventEmitter<IccColumnWidth[]>();
 
+  constructor() {
+    effect(() => {
+      if (this.columnConfigs()) {
+        this.columns.set(this.columnConfigs());
+      }
+      const widthRatio = viewportWidthRatio(this.gridConfig(), this.gridSetting(), this.columns());
+      this.setColumWidths(this.columns(), widthRatio);
+    });
+  }
+
   onColumnResizing(columnWidths: IccColumnWidth[]): void {
     this.setColumWidths(columnWidths, 1);
-    if (this.gridConfig.horizontalScroll) {
+    if (this.gridConfig().horizontalScroll) {
       this.tableWidth = getTableWidth(columnWidths);
     }
   }
 
   onColumnResized(columnWidths: IccColumnWidth[]): void {
-    const widthRatio = viewportWidthRatio(this.gridConfig, this.gridSetting, this.columns);
-    const columns: IccColumnConfig[] = [...this.columns].map((column, index) => ({
+    const widthRatio = viewportWidthRatio(this.gridConfig(), this.gridSetting(), this.columns());
+    const columns: IccColumnConfig[] = [...this.columns()].map((column, index) => ({
       ...column,
       width: columnWidths[index].width / widthRatio,
     }));
     this.setColumWidths(columnWidths, widthRatio);
-    this.gridFacade.setGridColumnsConfig(this.gridConfig, this.gridSetting, columns);
+    this.gridFacade.setGridColumnsConfig(this.gridConfig(), this.gridSetting(), columns);
   }
 
   onColumnDragDrop(events: DragDropEvent): void {
     const previousIndex = this.indexCorrection(events.previousIndex);
     const currentIndex = this.indexCorrection(events.currentIndex);
-    if (this.gridConfig.groupHeader) {
+    if (this.gridConfig().groupHeader) {
       this.moveGroupHeader(previousIndex, currentIndex);
     } else {
       this.moveColumn(previousIndex, currentIndex);
@@ -86,8 +76,8 @@ export class IccGridHeaderViewComponent {
   }
 
   private moveGroupHeader(previousIndex: number, currentIndex: number): void {
-    const moved = this.columns[previousIndex];
-    const changed = this.columns[currentIndex];
+    const moved = this.columns()[previousIndex];
+    const changed = this.columns()[currentIndex];
     if (!moved.groupHeader && !changed.groupHeader) {
       this.moveColumn(previousIndex, currentIndex);
     } else if (moved.groupHeader?.name === changed.groupHeader?.name) {
@@ -109,10 +99,10 @@ export class IccGridHeaderViewComponent {
         this.moveColumn(previousIndex, lastIndex);
       }
     } else if (moved.groupHeader && changed.groupHeader) {
-      const columns = [...this.columns];
+      const columns = [...this.columns()];
       if (currentIndex < previousIndex) {
         let newIndex = currentIndex;
-        this.columns.forEach((col, index) => {
+        this.columns().forEach((col, index) => {
           if (col.groupHeader?.name === moved.groupHeader?.name) {
             moveItemInArray(columns, index, newIndex);
             newIndex++;
@@ -120,40 +110,42 @@ export class IccGridHeaderViewComponent {
         });
       } else if (currentIndex > previousIndex) {
         let newIndex = previousIndex;
-        this.columns.forEach((col, index) => {
+        this.columns().forEach((col, index) => {
           if (col.groupHeader?.name === changed.groupHeader?.name) {
             moveItemInArray(columns, index, newIndex);
             newIndex++;
           }
         });
       }
-      this.gridFacade.setGridColumnsConfig(this.gridConfig, this.gridSetting, columns);
-      this.columns = [...columns];
+      this.gridFacade.setGridColumnsConfig(this.gridConfig(), this.gridSetting(), columns);
+      this.columns.set(columns);
     }
   }
 
   private firstIndex(name: string): number {
-    return this.columns.findIndex((col) => col.groupHeader?.name === name);
+    return this.columns().findIndex((col) => col.groupHeader?.name === name);
   }
 
   private lastIndex(name: string): number {
-    const index = [...this.columns]
+    const index = [...this.columns()]
       .slice()
       .reverse()
       .findIndex((col) => col.groupHeader?.name === name);
-    return this.columns.length - 1 - index;
+    return this.columns().length - 1 - index;
   }
 
   private moveColumn(previousIndex: number, currentIndex: number): void {
-    const columns = [...this.columns];
+    const columns = [...this.columns()];
     moveItemInArray(columns, previousIndex, currentIndex);
-    this.gridFacade.setGridColumnsConfig(this.gridConfig, this.gridSetting, columns);
+    this.gridFacade.setGridColumnsConfig(this.gridConfig(), this.gridSetting(), columns);
   }
 
   private setColumWidths(columns: IccColumnConfig[], widthRatio: number): void {
-    this.tableWidth = this.gridConfig.horizontalScroll ? getTableWidth(this.columns) : this.gridSetting.viewportWidth;
+    this.tableWidth = this.gridConfig().horizontalScroll
+      ? getTableWidth(this.columns())
+      : this.gridSetting().viewportWidth;
     const displayColumns = [...columns].filter((column) => column.hidden !== true);
-    let tot = this.gridConfig.rowSelection ? ROW_SELECTION_CELL_WIDTH : 0;
+    let tot = this.gridConfig().rowSelection ? ROW_SELECTION_CELL_WIDTH : 0;
     this.columnWidths = displayColumns.map((column, index) => {
       let width = Math.round(widthRatio * column.width!);
       tot += width;
@@ -168,7 +160,7 @@ export class IccGridHeaderViewComponent {
   }
 
   private indexCorrection(idx: number): number {
-    this.columns.forEach((column, index) => {
+    this.columns().forEach((column, index) => {
       if (column.hidden && idx >= index) {
         idx++;
       }
