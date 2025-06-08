05c4e14087837fb0c23f8d288721288a0b636bbf
Jimmy Lu <lujian98@yahoo.com>
2025-06-07 21:47:12 -0500

    grid signal

--- projects/icc/grid/src/components/grid-row/grid-row.component.ts
+++ projects/icc/grid/src/components/grid-row/grid-row.component.ts
@@ -1,56 +1,37 @@
-import { CommonModule } from '@angular/common';
-import { ChangeDetectionStrategy, Component, Input } from '@angular/core';
+import { ChangeDetectionStrategy, Component, input } from '@angular/core';
 import { ROW_SELECTION_CELL_WIDTH } from '../../models/constants';
 import { IccColumnConfig, IccColumnWidth, IccGridConfig, IccGridSetting } from '../../models/grid-column.model';
-import { IccRowSelectComponent } from '../row-select/row-select.component';
 import { IccGridCellEditComponent } from '../grid-cell/grid-cell-edit/grid-cell-edit.component';
 import { IccGridCellViewComponent } from '../grid-cell/grid-cell-view/grid-cell-view.component';
 import { IccGridCellComponent } from '../grid-cell/grid-cell.component';
+import { IccRowSelectComponent } from '../row-select/row-select.component';
 
 @Component({
   selector: 'icc-grid-row',
   templateUrl: './grid-row.component.html',
   styleUrls: ['./grid-row.component.scss'],
   changeDetection: ChangeDetectionStrategy.OnPush,
-  imports: [
-    CommonModule,
-    IccGridCellComponent,
-    IccGridCellViewComponent,
-    IccGridCellEditComponent,
-    IccRowSelectComponent,
-  ],
+  imports: [IccGridCellComponent, IccGridCellViewComponent, IccGridCellEditComponent, IccRowSelectComponent],
 })
 export class IccGridRowComponent<T> {
-  private _record!: T;
-  @Input() columns: IccColumnConfig[] = [];
-  @Input() gridSetting!: IccGridSetting;
-  @Input() gridConfig!: IccGridConfig;
-  @Input() rowIndex!: number;
-  @Input() selected = false;
-  @Input() columnWidths: IccColumnWidth[] = [];
-
-  @Input()
-  set record(data: T) {
-    this._record = data;
-  }
-  get record(): T {
-    return this._record;
-  }
+  columns = input.required<IccColumnConfig[]>();
+  gridSetting = input.required<IccGridSetting>();
+  gridConfig = input.required<IccGridConfig>();
+  rowIndex = input.required<number>();
+  selected = input.required<boolean>();
+  columnWidths = input.required<IccColumnWidth[]>();
+  record = input.required<T>();
 
   get selectColumnWidth(): string {
     return `${ROW_SELECTION_CELL_WIDTH}px`;
   }
 
   getColumnWidth(column: IccColumnConfig): string {
-    const width = this.columnWidths.find((col) => col.name === column.name)?.width;
+    const width = this.columnWidths().find((col) => col.name === column.name)?.width;
     return width ? `${width}px` : '';
   }
 
   isCellEditable(column: IccColumnConfig): boolean {
-    return !!(this.gridSetting.gridEditable && column.cellEditable);
-  }
-
-  trackByIndex(index: number): number {
-    return index;
+    return !!(this.gridSetting().gridEditable && column.cellEditable);
   }
 }
