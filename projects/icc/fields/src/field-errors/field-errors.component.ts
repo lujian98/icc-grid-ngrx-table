9508b6e290a5f240b955bd3842b3207213cc223a
Jimmy Lu <lujian98@yahoo.com>
2025-06-10 18:40:10 -0500

    fields signal

--- projects/icc/fields/src/field-errors/field-errors.component.ts
+++ projects/icc/fields/src/field-errors/field-errors.component.ts
@@ -1,30 +1,20 @@
-import { CommonModule } from '@angular/common';
-import { ChangeDetectionStrategy, Component, Input } from '@angular/core';
+import { ChangeDetectionStrategy, Component, input } from '@angular/core';
 import { ValidationErrors } from '@angular/forms';
 import { IccErrorDirective } from '@icc/ui/form-field';
 import { IccIconModule } from '@icc/ui/icon';
 import { TranslatePipe } from '@ngx-translate/core';
-import { IccFormFieldError } from './models/field-errors.model';
 
 @Component({
   selector: 'icc-field-errors',
   templateUrl: './field-errors.component.html',
   styleUrls: ['./field-errors.component.scss'],
   changeDetection: ChangeDetectionStrategy.OnPush,
-  imports: [CommonModule, TranslatePipe, IccIconModule, IccErrorDirective],
+  imports: [TranslatePipe, IccIconModule, IccErrorDirective],
 })
 export class IccFieldsErrorsComponent {
-  private _errors!: IccFormFieldError[];
-
-  @Input()
-  set errors(errors: ValidationErrors) {
-    this._errors = Object.keys(errors).map((key) => ({ type: key, ...errors[key] }));
-  }
-  get errors(): IccFormFieldError[] {
-    return this._errors;
-  }
-
-  trackByIndex(index: number): number {
-    return index;
-  }
+  errors = input.required({
+    transform: (errors: ValidationErrors) => {
+      return Object.keys(errors).map((key) => ({ type: key, ...errors[key] }));
+    },
+  });
 }
