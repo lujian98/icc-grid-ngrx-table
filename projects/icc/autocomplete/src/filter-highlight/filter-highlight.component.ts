2c92b2c735fb6ea64da3793cabb9ba512f77bc9e
Jimmy Lu <jimmy.lu@sunbirddcim.com>
2025-06-11 15:00:58 -0500

    field signal

--- projects/icc/autocomplete/src/filter-highlight/filter-highlight.component.ts
+++ projects/icc/autocomplete/src/filter-highlight/filter-highlight.component.ts
@@ -1,53 +1,29 @@
-import { CommonModule } from '@angular/common';
-import { ChangeDetectorRef, ChangeDetectionStrategy, Component, Input, inject } from '@angular/core';
+import { ChangeDetectionStrategy, Component, computed, input } from '@angular/core';
 
 @Component({
   selector: 'icc-filter-highlight',
   templateUrl: './filter-highlight.component.html',
   changeDetection: ChangeDetectionStrategy.OnPush,
-  imports: [CommonModule],
 })
 export class IccFilterHighlightComponent<T> {
-  private changeDetectorRef = inject(ChangeDetectorRef);
-  private _value: string = '';
-  private _filterValue: string = '';
-  values: string[] = [];
-
-  @Input()
-  set value(val: T) {
-    this._value = val as string;
-    this.setValues();
-  }
-  get value(): string {
-    return this._value;
-  }
-
-  @Input()
-  set filterValue(val: string) {
-    this._filterValue = val;
-    this.setValues();
-  }
-  get filterValue(): string {
-    return this._filterValue;
-  }
-
-  private setValues(): void {
-    if (this.filterValue && this.value) {
-      const pattern = this.filterValue
+  value = input.required<string>();
+  filterValue = input.required<string>();
+  values$ = computed(() => {
+    if (this.filterValue() && this.value()) {
+      const pattern = this.filterValue()
         .replace(/[\-\[\]\/\{\}\(\)\*\+\?\.\\\^\$\|]/g, '\\$&')
         .split(' ')
         .filter((t) => t.length > 0)
         .join('|');
       const regex = new RegExp(pattern, 'gi');
-      const marked = this.value.replace(regex, (match) => `<mark>${match}<mark>`);
-      this.values = marked.split('<mark>');
+      const marked = this.value().replace(regex, (match) => `<mark>${match}<mark>`);
+      return marked.split('<mark>');
     } else {
-      this.values = [this.value];
+      return [this.value()];
     }
-    this.changeDetectorRef.markForCheck();
-  }
+  });
 
   isMarkedValue(value: string): boolean {
-    return !!value && !!this.filterValue && value.toLowerCase() === this.filterValue.toLowerCase();
+    return !!value && !!this.filterValue() && value.toLowerCase() === this.filterValue().toLowerCase();
   }
 }
