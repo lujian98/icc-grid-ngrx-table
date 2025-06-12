482dc4dd80fd59eddf30e99cbab9b5896cd22276
Jimmy Lu <jimmy.lu@sunbirddcim.com>
2025-06-12 13:15:43 -0500

    form

--- projects/icc-demo/src/app/views/form/form.routes.ts
+++ projects/icc-demo/src/app/views/form/form.routes.ts
@@ -24,7 +24,7 @@ export const AppFormRoutes: Routes = [
       { path: 'file-select-upload', component: AppFileSelectUploadComponent },
       {
         path: '**',
-        redirectTo: 'simple-form',
+        redirectTo: 'form-page',
       },
     ],
   },
