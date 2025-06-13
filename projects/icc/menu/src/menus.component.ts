548bf193debfe20becabf743f5c230775b1f9a4c
Jimmy Lu <jimmy.lu@sunbirddcim.com>
2025-06-13 08:49:05 -0500

    menus signal

--- projects/icc/menu/src/menus.component.ts
+++ projects/icc/menu/src/menus.component.ts
@@ -1,4 +1,13 @@
-import { ChangeDetectionStrategy, Component, EventEmitter, Input, input, OnDestroy, Output } from '@angular/core';
+import {
+  ChangeDetectionStrategy,
+  Component,
+  EventEmitter,
+  Input,
+  signal,
+  input,
+  OnDestroy,
+  Output,
+} from '@angular/core';
 import { FormControl, FormGroup, FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { IccDisabled } from '@icc/ui/core';
 import { IccIconModule } from '@icc/ui/icon';
@@ -17,13 +26,14 @@ import { IccMenuConfig } from './models/menu-item.model';
   imports: [ReactiveFormsModule, FormsModule, IccIconModule, IccMenuItemComponent, IccPopoverDirective],
 })
 export class IccMenusComponent<T> implements OnDestroy {
-  private _items: IccMenuConfig[] = [];
   private selected: IccMenuConfig | undefined;
   private destroy$ = new Subject<void>();
-  private _values: { [key: string]: boolean } = {};
+  private isFirstTime: boolean = true;
   bottom = IccPosition.BOTTOM;
   rightBottom = IccPosition.RIGHTBOTTOM;
   hoverTrigger = IccTrigger.HOVER;
+  items$ = signal<IccMenuConfig[]>([]);
+  values$ = signal<{ [key: string]: boolean }>({});
 
   @Input() form: FormGroup | undefined;
   disabled = input<IccDisabled[]>([]);
@@ -31,25 +41,26 @@ export class IccMenusComponent<T> implements OnDestroy {
   clickToClose = input<boolean>(false);
   menuTrigger = input<IccTrigger>(IccTrigger.CLICK);
 
-  @Input()
-  set items(val: IccMenuConfig[]) {
-    this._items = val;
-    this.setItems();
-  }
-  get items(): IccMenuConfig[] {
-    return this._items;
-  }
+  items = input([], {
+    transform: (items: IccMenuConfig[]) => {
+      this.items$.set(items);
+      this.setItems(items);
+      return items;
+    },
+  });
 
-  @Input()
-  set values(values: { [key: string]: boolean }) {
-    if (this.form && values) {
-      this.form.patchValue({ ...values }, { emitEvent: false });
-    }
-    this._values = values;
-  }
-  get values(): { [key: string]: boolean } {
-    return this._values;
-  }
+  values = input(
+    {},
+    {
+      transform: (values: { [key: string]: boolean }) => {
+        this.values$.set(values);
+        if (this.form && values) {
+          this.form.patchValue({ ...values }, { emitEvent: false });
+        }
+        return values;
+      },
+    },
+  );
 
   @Output() iccMenuItemClick = new EventEmitter<IccMenuConfig>(false);
   @Output() iccMenuFormChanges = new EventEmitter<T>(false);
@@ -74,9 +85,15 @@ export class IccMenusComponent<T> implements OnDestroy {
     return !item.hidden && !!item.children && item.children.length > 0;
   }
 
-  private setItems(): void {
+  private setItems(items: IccMenuConfig[]): void {
     if (this.selected) {
-      this.items.forEach((item) => (item.selected = item.name === this.selected!.name));
+      const items = this.items$().map((item) => {
+        return {
+          ...item,
+          selected: item.name === this.selected!.name,
+        };
+      });
+      this.items$.set(items);
     }
     if (!this.form) {
       this.form = new FormGroup({});
@@ -84,10 +101,11 @@ export class IccMenusComponent<T> implements OnDestroy {
         .pipe(debounceTime(100), distinctUntilChanged(), takeUntil(this.destroy$))
         .subscribe((val) => {
           this.iccMenuFormChanges.emit(this.form?.value);
+          this.values$.set(this.form?.value);
           this.values = this.form?.value;
         });
     }
-    this.items.forEach((item) => {
+    items.forEach((item) => {
       // TODO disabled ??
       const field = this.form!.get(item.name);
       if (item.checkbox && !field) {
@@ -97,7 +115,13 @@ export class IccMenusComponent<T> implements OnDestroy {
   }
 
   private setSelected(selectedItem: IccMenuConfig): void {
-    this.items.forEach((item) => (item.selected = item.name === selectedItem.name));
+    const items = this.items$().map((item) => {
+      return {
+        ...item,
+        selected: item.name === selectedItem.name,
+      };
+    });
+    this.items$.set(items);
     this.selected = selectedItem;
   }
 
